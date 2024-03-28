24aee9956f27871ae37f1a45964a991fe985c348
PeaceEhi <peacejeremiah17@gmail.com>
2024-03-28 10:27:51 +0100

    Room Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
 import NavBar from './Components/NavBar/NavBar';
+import Rooms from './Components/Rooms/Rooms';
+import Title from './Components/Title/Title';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Rooms/>
+      <Title title='' subTitle='HIGH CLASS LUXURY'/>
     </div>
   );
 }
