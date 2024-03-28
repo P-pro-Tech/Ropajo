c92fcae8443ac6ac594c8244825c41a706222683
progress-okoro <progress.okoro@binghamuni.edu.ng>
2024-03-28 12:34:47 +0100

    guest id finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
+import GuestId from './Components/GuestId/GuestId';
 import NavBar from './Components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <GuestId/>
     </div>
   );
 }
--- src/Components/GuestId/GuestId.jsx
+++ src/Components/GuestId/GuestId.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './GuestId.css'
+import guest_img from '../../assets/Ropa2.jpeg'
 
 const GuestId = () => {
   return (
-    <div>
-      
+    <div className='guest-id'>
+      <div className="guest-img-holder">
+        <img src={guest_img} alt="" />
+      </div>
+      <div className="guest-des-holder">
+        <form className='guest-form'>
+            <fieldset>
+                <legend>Enter your unique Guest ID</legend>
+                <input type="text" name="" id="" placeholder='#R1410X'/>
+            </fieldset>
+            <button type="submit">Enter Guest ID</button>
+        </form>
+      </div>
     </div>
   )
 }
