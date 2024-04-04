e5f5a28a4f125e66499618edab6b0d471ede1079
progress-okoro <progress.okoro@binghamuni.edu.ng>
2024-04-04 11:41:09 +0100

    adjusted coffee h3 width

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import GuestId from './Components/GuestId/GuestId';
 import Located from './Components/Located/Located';
 import NavBar from './Components/NavBar/NavBar';
 import Restaurant from './Components/Restaurant/Restaurant';
-import Pool from './Components/Pool/Pool';
+import Pool from './Components/Pool/Pool';  
 import Rooms from './Components/Rooms/Rooms';
 import Gallery from './Components/Gallery/Gallery';
 import Contact from './Components/Contact/Contact';
