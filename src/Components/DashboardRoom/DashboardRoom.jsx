0eda33cb0d0cff20ff6cf26f207ada3e6a96629b
PeaceEhi <peacejeremiah17@gmail.com>
2024-04-12 16:23:50 +0100

    dashboard rooms main

--- src/Components/DashboardRoom/DashboardRoom.jsx
+++ src/Components/DashboardRoom/DashboardRoom.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './DashboardRoom.css'
 import{Link} from 'react-scroll'
 import book_logo from '../../assets/mob-logo.svg';
@@ -6,6 +6,14 @@ import mob_menu from '../../assets/mob-menu.svg'
 import play_icon from '../../assets/play-icon.svg';
 
 const DashboardRoom = () => {
+  const [isActive1, setIsActive1] = useState(false);
+  const [isActive2, setIsActive2] = useState(false);
+  const [isActive3, setIsActive3] = useState(false);
+
+  // const toggleDropdown = (dropdownNumber, setActiveFunction) => {
+  //   setActiveFunction((prevState) => !prevState);
+  // };
+  
   return (
     <div className='dashboard-room'>
 
@@ -25,95 +33,149 @@ const DashboardRoom = () => {
           <img src={mob_menu} alt="" className='dbr-mob-menu' />
         </div>
 
+        <div className='dropdown-table'>
+          <div className='dropdown'>
+          <div className="dropdown-btn">
+          <div className="dropdown-item-left">
+          <img
+                src={play_icon}
+                alt=""
+                className={isActive1 ? 'dropdown-icon rotate-icon' : 'dropdown-icon'}
+                onClick={() => setIsActive1(!isActive1)}
+              />
+            {/* <img src={play_icon} alt="" onClick={(e) =>
+          setIsActive1(!isActive1)} /> */}
+          Room No:</div>
+              <div className="dropdown-item-right">256</div>
+          </div>
+          {isActive1 && (
+            <div className="dropdown-content">
+            
+            <div className="dropdown-item">
+            <div className="dropdown-item-left">Date:</div>
+              <div className="dropdown-item-right">13.04.2024</div>
+            </div>
 
-        <div className='drp-table'>
-          <div className="drp">
-            <table>
-              <thead>
-                <tr>
-                  <th><img src={play_icon} alt="" />Room No:</th>
-                  <th>256</th>
-                </tr>
-                <tr>
-                  <td>Date:</td>
-                  <td>13.04.2024</td>
-                </tr>
-                <tr>
-                  <td>Check-in:</td>
-                  <td>06:47pm</td>
-                </tr>
-                <tr>
-                  <td>Check-out:</td>
-                  <td>
+            <div className="dropdown-item">
+            <div className="dropdown-item-left">Check-in:</div>
+              <div className="dropdown-item-right">06:47pm</div>
+            </div>
+
+            <div className="dropdown-item">
+              <div className="dropdown-item-left">Check-out:</div>
+              <div className="dropdown-item-right">
                     <p>13.04.2024</p>
                     <p>12:00pm</p>
-                  </td>
-                </tr>
-                <tr>
-                    <td>Amount:</td>
-                    <td>30,000.00</td>
-                  </tr>
-              </thead>
-            </table>
+              </div>
+            </div>
+
+            <div className="dropdown-item">
+            <div className="dropdown-item-left">Amount:</div>
+              <div className="dropdown-item-right">30,000.00</div>
+            </div>
+
           </div>
-          <div className="drp">
-          <table>
-              <thead>
-                <tr>
-                  <th><img src={play_icon} alt="" />Room No:</th>
-                  <th>256</th>
-                </tr>
-                <tr>
-                  <td>Date:</td>
-                  <td>13.04.2024</td>
-                </tr>
-                <tr>
-                  <td>Check-in:</td>
-                  <td>06:47pm</td>
-                </tr>
-                <tr>
-                  <td>Check-out:</td>
-                  <td>
+          )}
+          </div>
+
+
+          <div className='dropdown'>
+          <div className="dropdown-btn">
+          <div className="dropdown-item-left"> 
+          <img
+                src={play_icon}
+                alt=""
+                className={isActive2 ? 'dropdown-icon rotate-icon' : 'dropdown-icon'}
+                onClick={() => setIsActive2(!isActive2)}
+              />
+
+          {/* <img src={play_icon} alt="" onClick={(e) =>
+          setIsActive2(!isActive2)}/> */}
+          Room No:</div>
+              <div className="dropdown-item-right">256</div>
+          </div>
+          {isActive2 && (
+            <div className="dropdown-content">
+            
+            <div className="dropdown-item">
+            <div className="dropdown-item-left">Date:</div>
+              <div className="dropdown-item-right">13.04.2024</div>
+            </div>
+
+            <div className="dropdown-item">
+            <div className="dropdown-item-left">Check-in:</div>
+              <div className="dropdown-item-right">06:47pm</div>
+            </div>
+
+            <div className="dropdown-item">
+              <div className="dropdown-item-left">Check-out:</div>
+              <div className="dropdown-item-right">
                     <p>13.04.2024</p>
                     <p>12:00pm</p>
-                  </td>
-                </tr>
-                <tr>
-                    <td>Amount:</td>
-                    <td>30,000.00</td>
-                  </tr>
-              </thead>
-            </table>
+              </div>
+            </div>
+
+            <div className="dropdown-item">
+            <div className="dropdown-item-left">Amount:</div>
+              <div className="dropdown-item-right">30,000.00</div>
+            </div>
+
           </div>
-          <div className="drp">
-          <table>
-              <thead>
-                <tr>
-                  <th><img src={play_icon} alt="" />Room No:</th>
-                  <th>256</th>
-                </tr>
-                <tr>
-                  <td>Date:</td>
-                  <td>13.04.2024</td>
-                </tr>
-                <tr>
-                  <td>Check-in:</td>
-                  <td>06:47pm</td>
-                </tr>
-                <tr>
-                  <td>Check-out:</td>
-                  <td>
+          )}
+          </div>
+
+
+
+          <div className='dropdown'>
+          <div className="dropdown-btn">
+          <div className="dropdown-item-left">
+          <img
+                src={play_icon}
+                alt=""
+                className={isActive3 ? 'dropdown-icon rotate-icon' : 'dropdown-icon'}
+                onClick={() => setIsActive3(!isActive3)}
+              />
+            
+            {/* <img src={play_icon} alt="" onClick={(e) =>
+          setIsActive3(!isActive3)}/> */}
+          Room No:</div>
+              <div className="dropdown-item-right">256</div>
+          </div>
+          {isActive3 && (
+            <div className="dropdown-content">
+            
+            <div className="dropdown-item">
+            <div className="dropdown-item-left">Date:</div>
+              <div className="dropdown-item-right">13.04.2024</div>
+            </div>
+
+            <div className="dropdown-item">
+            <div className="dropdown-item-left">Check-in:</div>
+              <div className="dropdown-item-right">06:47pm</div>
+            </div>
+
+            <div className="dropdown-item">
+              <div className="dropdown-item-left">Check-out:</div>
+              <div className="dropdown-item-right">
                     <p>13.04.2024</p>
                     <p>12:00pm</p>
-                  </td>
-                </tr>
-                <tr>
-                    <td>Amount:</td>
-                    <td>30,000.00</td>
-                  </tr>
-              </thead>
-            </table>
+              </div>
+            </div>
+
+            <div className="dropdown-item">
+            <div className="dropdown-item-left">Amount:</div>
+              <div className="dropdown-item-right">30,000.00</div>
+            </div>
+
           </div>
+          )}
+          </div>
+
+
+
+
+          
+          
         </div>
     </div>
   )
