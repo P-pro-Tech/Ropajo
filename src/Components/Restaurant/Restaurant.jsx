92194fbce09b47ceeda8caf9cc7351044a9db2fe
progress-okoro <progress.okoro@binghamuni.edu.ng>
2024-04-24 11:17:04 +0100

    just starting restaurant changes

--- src/Components/Restaurant/Restaurant.jsx
+++ src/Components/Restaurant/Restaurant.jsx
@@ -6,17 +6,22 @@ import { Link } from 'react-router-dom'
 const Restaurant = () => {
 return (
     <div className='restaurant'>
-    
-    <div className="empty"></div>
-    <div className="opacity">
-        <h2>Restaurant</h2>
-        <h3>HIGH CLASS LUXURY</h3>
-        <p>Right in the heart of the Food Basket of Nigeria
-            Ropajo Hotels perfectly unveil its touch of luxury
-            and creativity in a space.
-        </p>
-            <Link to='./Booking'><button>Book Now</button></Link>
-    </div>
+        <div className="empty">
+        </div>
+        <div className="opacity">
+            <div className=''>
+                <h2>Restaurant</h2>
+                <div className=''>
+                    <h3>HIGH CLASS LUXURY</h3>
+                    <p>Right in the heart of the Food Basket of Nigeria
+                        Ropajo Hotels perfectly unveil its touch of luxury
+                        and creativity in a space.</p>
+                </div>
+                <Link to='./Booking'>
+                    <button>Book Now</button>
+                </Link>
+            </div>
+        </div>
     </div>
 )
 }
