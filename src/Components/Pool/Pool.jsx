79df9f8150c1f28579e96edcc818d8cd5238d9be
progress-okoro <progress.okoro@binghamuni.edu.ng>
2024-04-25 13:04:42 +0100

    edited pool  mobile

--- src/Components/Pool/Pool.jsx
+++ src/Components/Pool/Pool.jsx
@@ -8,7 +8,7 @@ const Pool = () => {
     <div className='pool'>
       <div className="pool-content-left">
         <div className="content-left-div">
-            <h1>Pool & <br />Party</h1>
+            <h1>Pool & Party</h1>
               <div className='pool-bold-text'>
                 <h3>HIGH CLASS LUXUARY</h3>
                 <p>Right in the heart of the Food Basket of Nigeria Ropajo Hotels perfectly unveil its touch of luxury and
