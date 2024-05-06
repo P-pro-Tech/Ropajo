8dd1cc4c0941af35ec538694cdb3fb9d3d08ef9b
progress-okoro <progress.okoro@binghamuni.edu.ng>
2024-05-06 23:47:35 +0100

    latest view

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -6,40 +6,46 @@ import ropa_text from '../../assets/Layer_2.svg'
 
 const Contact = () => {
   return (
-    <div className='contact'>
-      <div className="logo">
-        <img src={ropa_logo} alt="" className='logo-img'/>
-        <img src={ropa_text} alt="" className='logo-text'/>
-        <p>Hotels and Apartments</p>
-      </div>
-      <div className="list">
+    <div className='main-contact'>
+      <div className='contact'>
+        <div className="logo">
+          <div className='cont-logo-insider'>
+            <div className='cont-logo-img'>
+            <img src={ropa_logo} alt="" className='logo-img'/>
+            <img src={ropa_text} alt="" className='logo-text'/>
+            </div>
+            <p>Hotels and Apartments</p>
+          </div>
+        </div>
+        <div className="list">
+          <ul>
+            <li>Home</li>
+            <li>FAQ</li>
+            <li>Privacy POlicy</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div className="list">
         <ul>
-          <li>Home</li>
-          <li>FAQ</li>
-          <li>Privacy POlicy</li>
-          <li>Contact</li>
-        </ul>
-      </div>
-      <div className="list">
-      <ul>
-          <li>Home</li>
-          <li>FAQ</li>
-          <li>Privacy POlicy</li>
-          <li>Contact</li>
-        </ul>
-      </div>
-      <div className='address'>
-        <div className='contact-details'>
-          <h4>Have questions and suggestions?</h4>
-          <p>hotels.ropajo@gmail.com</p>
+            <li>Home</li>
+            <li>FAQ</li>
+            <li>Privacy POlicy</li>
+            <li>Contact</li>
+          </ul>
         </div>
-        <div className='contact-details'>
-          <h4>Need assistance? Give us a call</h4>
-          <p>+234 803 320 2272</p>
-        </div>
-        <div className='contact-details'>
-          <h4>Our Location</h4>
-          <p>Lobi Quarters, beside EFCC Office, Makurdi</p>
+        <div className='address'>
+          <div className='contact-details'>
+            <h4>Have questions and suggestions?</h4>
+            <p>hotels.ropajo@gmail.com</p>
+          </div>
+          <div className='contact-details'>
+            <h4>Need assistance? Give us a call</h4>
+            <p>+234 803 320 2272</p>
+          </div>
+          <div className='contact-details'>
+            <h4>Our Location</h4>
+            <p>Lobi Quarters, beside EFCC Office, Makurdi</p>
+          </div>
         </div>
       </div>
     </div>
