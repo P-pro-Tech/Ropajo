8b68d2b9888e47226104116da4cdec2066eeea32
okoro-progress <progressewokoro@gmail.com>
2024-06-18 12:58:01 +0100

    changed all tr to div in style all DAcc

--- src/Components/DashboardAccount/DashboardAccount.jsx
+++ src/Components/DashboardAccount/DashboardAccount.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import './DashboardAccount.css'
 import GuestDashBoard from '../GuestDashBoard/GuestDashBoard'
 import { Link } from 'react-router-dom'
+import DashBoardHeader from '../DashBoardHeader/DashBoardHeader'
+import roll_icon from '../../assets/play-icon.svg'
 
 const DashboardAccount = () => {
 return (
-    <div className='dashboardAccount'>
+<div className='dashboardAccount'>    
+    <div className='dashboardAccount-web'>
         <div className='dashAccount-guest-section'>
             <GuestDashBoard/>
         </div>
@@ -22,57 +25,57 @@ return (
                     <div className='dashAccount-web-table-holder'>
                     <table className='dashAccount-web-table'>
                         <thead>
-                        <tr>
-                            <th>Date</th>
-                            <th>Category</th>
-                            <th>Bill</th>
-                            <th>Paid</th>
-                            <th>Balance</th>
-                        </tr>
+                        <div>
+                            <span>Date</span>
+                            <span>Category</span>
+                            <span>Bill</span>
+                            <span>Paid</span>
+                            <span>Balance</span>
+                        </div>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>03.02.2023</td>
-                            <td>Room</td>
-                            <td>90,000.00</td>
-                            <td>40,000.00</td>
-                            <td>50,000.00</td>
-                        </tr>
-                        <tr>
-                            <td>03.02.2023</td>
-                            <td>Laundry</td>
-                            <td>25,200.00</td>
-                            <td>15,000.00</td>
-                            <td>10,000.00</td>
-                        </tr>
-                        <tr>
-                            <td>03.02.2023</td>
-                            <td>Restaurant</td>
-                            <td>9,500.00</td>
-                            <td>9,500.00</td>
-                            <td>0.00</td>
-                        </tr>
-                        <tr>
-                            <td>03.02.2023</td>
-                            <td>Pool</td>
-                            <td>5,000.00</td>
-                            <td>15,000.00</td>
-                            <td>10,000.00</td>
-                        </tr>
-                        <tr>
-                            <td>03.02.2023</td>
-                            <td>Room</td>
-                            <td>30,000.00</td>
-                            <td>00.00</td>
-                            <td>30,000.00</td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td><h3 className='dashAccount-total'>TOTAL</h3></td>
-                            <td><span className='dashAccount-total-amount'>216,500.00</span></td>
-                        </tr>
+                        <div>
+                            <span>03.02.2023</span>
+                            <span>Room</span>
+                            <span>90,000.00</span>
+                            <span>40,000.00</span>
+                            <span>50,000.00</span>
+                        </div>
+                        <div>
+                            <span>03.02.2023</span>
+                            <span>Laundry</span>
+                            <span>25,200.00</span>
+                            <span>15,000.00</span>
+                            <span>10,000.00</span>
+                        </div>
+                        <div>
+                            <span>03.02.2023</span>
+                            <span>Restaurant</span>
+                            <span>9,500.00</span>
+                            <span>9,500.00</span>
+                            <span>0.00</span>
+                        </div>
+                        <div>
+                            <span>03.02.2023</span>
+                            <span>Pool</span>
+                            <span>5,000.00</span>
+                            <span>15,000.00</span>
+                            <span>10,000.00</span>
+                        </div>
+                        <div>
+                            <span>03.02.2023</span>
+                            <span>Room</span>
+                            <span>30,000.00</span>
+                            <span>00.00</span>
+                            <span>30,000.00</span>
+                        </div>
+                        <div>
+                            <span></span>
+                            <span><h3 className='dashAccount-total'>TOTAL</h3></span>
+                            <span><span className='dashAccount-total-amount bill-red'>216,500.00</span></span>
+                            <span ><span className='dashAccount-total-amount paid-green'>216,500.00</span></span>
+                            <span><span className='dashAccount-total-amount'>216,500.00</span></span>
+                        </div>
                         </tbody>
                     </table>
                     </div>
@@ -81,6 +84,15 @@ return (
             </div>
         </div>
     </div>
+
+    {/* mobile deign start here */}
+    <div className='mobile-dasboardAccount'>
+        <div className='mobile-dasboardAccount-head'>
+            <DashBoardHeader/>
+        </div>
+        
+    </div>
+</div>    
 )
 }
 
