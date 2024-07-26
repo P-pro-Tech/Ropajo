307a6d01398210eebb42766c939c6720551936f6
Peace Jerry <peacejeremiah17@gmail.com>
2024-07-26 17:57:02 +0100

    fixed red error from GuestDashboard

--- src/Components/Staff-Receptionist/ReceptionAccount/ReceptionAccount.jsx
+++ src/Components/Staff-Receptionist/ReceptionAccount/ReceptionAccount.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import './ReceptionAccount.css'
-import GuestDashBoard from "../../Guest/GuestDashBoard/GuestDashBoard";
 import { Link } from "react-router-dom";
 import DashBoardHeader from "../../DashBoardHeader/DashBoardHeader";
 import roll_icon from "../../../assets/play-icon.svg";
