6c29003853f24a7d13e8de3d0a9f287a7a26d02d
okoro-progress <progressewokoro@gmail.com>
2024-08-03 15:40:58 +0100

    all new update

--- frontend/src/Components/Staff-Receptionist/RegisterGuests/RegisterGuests.jsx
+++ frontend/src/Components/Staff-Receptionist/RegisterGuests/RegisterGuests.jsx
@@ -8,6 +8,7 @@ import { convertToBase64 } from "../../../utils/convertImg";
 import { useAddGuestMutation } from "../../../hooks/guest";
 import toast from "react-hot-toast";
 import { shuffleString } from "../../../utils/generateId";
+import { Link } from "react-router-dom";
 
 const RegisterGuests = () => {
   const {
@@ -81,156 +82,185 @@ const RegisterGuests = () => {
   };
 
   return (
-    <div className='registerGuests'>
-      <div className='registerGuests-hue'>
-        <div className='registerGuests-head'>
+    <div className="registerGuests">
+      <div className="registerGuests-hue">
+        <div className="registerGuests-head">
           <GuestReservationHeader />
         </div>
-        <div className='registerGuests-mobile-head'>
+        <div className="registerGuests-mobile-head">
           <DashBoardHeader />
         </div>
-        <div className='registerGuests-body'>
-          <div className='registerGuests-body-content-holder'>
+        <div className="registerGuests-body">
+          <div className="registerGuests-body-content-holder">
             <h1>Register Guests</h1>
-            <form onSubmit={handleSubmit} className='registerGuests-form'>
-              <div className='registerGuests-form-holder'>
-                <input
-                  onChange={(e) => setFullName(e.target.value)}
-                  value={fullName}
-                  type='text'
-                  placeholder='Guest Name:'
-                  className='registerGuests-form50p'
-                />
-                <div className='registerGuests-form50p registerGuests-passport'>
+            <form onSubmit={handleSubmit} className="registerGuests-form">
+              <div className="registerGuests-form-holder">
+                <label htmlFor="">
+                  Guest Name:
+                  <input
+                    onChange={(e) => setFullName(e.target.value)}
+                    value={fullName}
+                    type="text"
+                    placeholder="   Guest Name:"
+                    className="registerGuests-form50p"
+                  />
+                </label>
+                <div className="registerGuests-form50p registerGuests-passport">
                   <span>Upload ID Card:</span>
                   <input
-                    type='file'
+                    type="file"
                     onChange={(e) => uploadFile(e)}
-                    id='registerGuests-passport'
-                    placeholder='Upload ID:'
+                    id="registerGuests-passport"
+                    placeholder="Upload ID:"
                     hidden
                   />
                   <label
-                    htmlFor='registerGuests-passport'
-                    className='registerGuests-file-label'
+                    htmlFor="registerGuests-passport"
+                    className="registerGuests-file-label"
                   >
                     upload
                   </label>
                 </div>
               </div>
-              <div className='registerGuests-form-holder'>
-                <input
-                  type='text'
-                  placeholder='Address:'
-                  onChange={(e) => setAddress(e.target.value)}
-                  value={address}
-                  className='registerGuests-form50p'
-                />
-                <input
-                  type='text'
-                  placeholder='Nationality:'
-                  onChange={(e) => setNationality(e.target.value)}
-                  value={Nationality}
-                  className='registerGuests-form50p'
-                />
+              <div className="registerGuests-form-holder">
+                <label  htmlFor="">Address:
+                  <input
+                    type="text"
+                    placeholder=""
+                    onChange={(e) => setAddress(e.target.value)}
+                    value={address}
+                    className="registerGuests-form50p"
+                  />
+                </label>
+                <label htmlFor="">Nationality:
+                  <input
+                    type="text"
+                    placeholder=""
+                    onChange={(e) => setNationality(e.target.value)}
+                    value={Nationality}
+                    className="registerGuests-form50p"
+                  />
+                </label>
               </div>
-              <div className='registerGuests-form-holder'>
-                <input
-                  type='email'
-                  placeholder='Email:'
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  className='registerGuests-form50p'
-                />
-                <input
-                  type='number'
-                  placeholder='Phone:'
-                  value={phoneNumber}
-                  onChange={(e) => setPhoneNumber(e.target.value)}
-                  className='registerGuests-form50p'
-                />
+              <div className="registerGuests-form-holder">
+                <label htmlFor="">Email:
+                  <input
+                    type="email"
+                    placeholder=""
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="registerGuests-form50p"
+                  />
+                </label>
+                <label htmlFor="">Phone:
+                  <input
+                    type="number"
+                    placeholder=""
+                    value={phoneNumber}
+                    onChange={(e) => setPhoneNumber(e.target.value)}
+                    className="registerGuests-form50p"
+                  />
+                </label>
               </div>
-              <div className='registerGuests-form-holder'>
-                <input
-                  type='text'
-                  placeholder='Occupation:'
-                  value={occupation}
-                  onChange={(e) => setOccupation(e.target.value)}
-                  className='registerGuests-form50p'
-                />
-                <input
-                  type='text'
-                  placeholder='Passport No:'
-                  value={passportNumber}
-                  onChange={(e) => setPassportNumber(e.target.value)}
-                  className='registerGuests-form50p'
-                />
+              <div className="registerGuests-form-holder">
+                <label htmlFor="">Occupation:
+                  <input
+                    type="text"
+                    placeholder=""
+                    value={occupation}
+                    onChange={(e) => setOccupation(e.target.value)}
+                    className="registerGuests-form50p"
+                  />
+                </label>
+                <label htmlFor="">Passport No:
+                  <input
+                    type="text"
+                    placeholder=""
+                    value={passportNumber}
+                    onChange={(e) => setPassportNumber(e.target.value)}
+                    className="registerGuests-form50p"
+                  />
+                </label>
               </div>
-              <div className='registerGuests-form-holder'>
-                <input
-                  type='text'
-                  placeholder='Car Reg No.'
-                  value={carRegNumber}
-                  onChange={(e) => setCarRegNumber(e.target.value)}
-                  className='registerGuests-form50p'
-                />
-                <input
-                  type='text'
-                  placeholder='Purpose of Travel:'
-                  value={purposeOfTravel}
-                  onChange={(e) => setPurposeOfTravel(e.target.value)}
-                  className='registerGuests-form50p'
-                />
+              <div className="registerGuests-form-holder">
+                <label htmlFor="">Car Reg No.
+                  <input
+                    type="text"
+                    placeholder=""
+                    value={carRegNumber}
+                    onChange={(e) => setCarRegNumber(e.target.value)}
+                    className="registerGuests-form50p"
+                  />
+                </label>
+                <label htmlFor="">Purpose of Travel:
+                  <input
+                    type="text"
+                    placeholder=""
+                    value={purposeOfTravel}
+                    onChange={(e) => setPurposeOfTravel(e.target.value)}
+                    className="registerGuests-form50p"
+                  />
+                </label>
               </div>
-              <div className='registerGuests-form-holder'>
-                <input
-                  type='text'
-                  placeholder='Arrival Date:'
-                  value={arriverDate}
-                  onChange={(e) => setArriverDate(e.target.value)}
-                  className='registerGuests-form50p'
-                  onFocus={(e) => (e.target.type = "date")}
-                  onBlur={(e) => (e.target.type = "text")}
-                />
-                <input
-                  type='text'
-                  placeholder='Departure Date:'
-                  value={departureDate}
-                  onChange={(e) => setDepartureDate(e.target.value)}
-                  className='registerGuests-form50p'
-                  onFocus={(e) => (e.target.type = "date")}
-                  onBlur={(e) => (e.target.type = "text")}
-                />
+              <div className="registerGuests-form-holder">
+                <label htmlFor="">Arrival Date:
+                  <input
+                    type="text"
+                    placeholder=""
+                    value={arriverDate}
+                    onChange={(e) => setArriverDate(e.target.value)}
+                    className="registerGuests-form50p"
+                    onFocus={(e) => (e.target.type = "date")}
+                    onBlur={(e) => (e.target.type = "text")}
+                  />
+                </label>
+                <label htmlFor="">Departure Date:
+                  <input
+                    type="text"
+                    placeholder=""
+                    value={departureDate}
+                    onChange={(e) => setDepartureDate(e.target.value)}
+                    className="registerGuests-form50p"
+                    onFocus={(e) => (e.target.type = "date")}
+                    onBlur={(e) => (e.target.type = "text")}
+                  />
+                </label>
               </div>
-              <div className='registerGuests-form-holder'>
-                <input
-                  type='text'
-                  value={arriverFrom}
-                  onChange={(e) => setArriverFrom(e.target.value)}
-                  placeholder='Arrival From:'
-                  className='registerGuests-form50p'
-                />
-                <input
-                  type='text'
-                  placeholder='Depart To:  '
-                  value={departTo}
-                  onChange={(e) => setDepartTo(e.target.value)}
-                  className='registerGuests-form50p'
-                />
+              <div className="registerGuests-form-holder">
+                <label htmlFor="">Arrival From:
+                  <input
+                    type="text"
+                    value={arriverFrom}
+                    onChange={(e) => setArriverFrom(e.target.value)}
+                    placeholder=""
+                    className="registerGuests-form50p"
+                  />
+                </label>
+                <label htmlFor="">Depart To:
+                  <input
+                    type="text"
+                    placeholder=""
+                    value={departTo}
+                    onChange={(e) => setDepartTo(e.target.value)}
+                    className="registerGuests-form50p"
+                  />
+                </label>
               </div>
-              <div className='registerGuests-form-holder'>
+              {/* <div className="registerGuests-form-holder"> */}
                 {/* <input type="button" value="Room" className='registerGuests-form25p' />
                             <input type="button" value="Pool" className='registerGuests-form25p' />
                             <input type="button" value="Restaurant" className='registerGuests-form25p' />
                             <input type="button" value="Laundry" className='registerGuests-form25p' /> */}
-              </div>
-              <div className='registerGuests-form-holder'>
-                <input
-                  type='submit'
-                  value='Register'
-                  className='registerGuests-form-100p'
-                />
-              </div>
+              {/* </div> */}
+              <Link to="">
+                <div className="registerGuests-form-holder">
+                  <input
+                    type="submit"
+                    value="Register"
+                    className="registerGuests-form-100p"
+                  />
+                </div>
+              </Link>
             </form>
           </div>
         </div>
